9121393428e4145241f8e0431dad85ac7bb69b26
Gregory Papapostolou <88238306+HackerXYT@users.noreply.github.com>
2026-04-04 02:15:38 +0300

    Production

--- login/register/script.js
+++ login/register/script.js
@@ -208,3 +208,25 @@ function signIn() {
     window.location.href = '../' + (isTazro ? '?login=tazro' : '')
 }
 
+const CLIENT_ID = "314460881843-5k3d8113ch5clklju6rhu23hgmilnsdv.apps.googleusercontent.com";
+const REDIRECT_URI = "https://evox.uno/login/auth.html";
+
+document.getElementById("googleLogin").onclick = () => {
+
+    if (isTazro) {
+        sessionStorage.setItem("tazroRedirect", "true");
+    }
+    const url =
+        "https://accounts.google.com/o/oauth2/v2/auth?" +
+        new URLSearchParams({
+            client_id: CLIENT_ID,
+            redirect_uri: REDIRECT_URI,
+            response_type: "code",
+            scope: "openid email profile",
+            access_type: "offline",
+            prompt: "consent"
+        });
+
+    window.location.href = url;
+
+};
